779ef2a454cd2ce8460f6765c3c5ac3d79dbe4e0
Dennis Fernandes <dennisfernandes0212@gmail.com>
2023-06-26 17:23:00 +0700

    kelar

--- script.js
+++ script.js
@@ -1,12 +1,43 @@
-// Klik mengubah warna background
+// Klik Toggle
+const buttonToggle = document.querySelector('.button-warna-toggle');
+const h1 = document.getElementsByTagName('h1')[0];
 
-const buttonBg = document.querySelector('.button-warna');
-const body = document.querySelector('body');
-
-buttonBg.addEventListener('click', function(){
-  const red = Math.floor(Math.random() * 255);
-  const green = Math.floor(Math.random() * 255);
-  const blue = Math.floor(Math.random() * 255);
-
-  body.style.backgroundColor = 'rgb'+ '(' + red + ',' + green + ',' + blue + ')'
+buttonToggle.addEventListener('click', function(){
+  h1.classList.toggle('bg-toggle');
 });
+
+
+
+
+// Warna Random
+const buttonRandom = document.createElement('button');
+const butonRandomText = document.createTextNode('Warna Random');
+
+buttonRandom.appendChild(butonRandomText);
+buttonToggle.after(buttonRandom);
+
+buttonRandom.addEventListener('click', function(){
+  const red = Math.round(Math.random() * 255 + 1);
+  const green = Math.round(Math.random() * 255 + 1);
+  const blue = Math.round(Math.random() * 255 + 1);
+
+  document.body.style.background = 'radial-gradient(circle, rgba('+ red +','+ green +','+ blue +',1) 0%, rgba('+ blue +','+ red +','+ green +',1) 100%)';
+});
+
+
+// Slider Random
+const sliderMerah = document.querySelector('.slider-merah');
+const sliderHijau = document.querySelector('.slider-hijau');
+const sliderBiru = document.querySelector('.slider-biru');
+
+sliderMerah.addEventListener('input', function(){
+  document.body.style.background = 'radial-gradient(circle, rgba('+ sliderMerah.value +','+ sliderHijau.value +','+ sliderBiru.value +',1) 0%, rgba('+ sliderBiru.value +','+ sliderMerah.value +','+ sliderHijau.value +',1) 100%)';
+})
+
+sliderHijau.addEventListener('input', function(){
+  document.body.style.background = 'radial-gradient(circle, rgba('+ sliderMerah.value +','+ sliderHijau.value +','+ sliderBiru.value +',1) 0%, rgba('+ sliderBiru.value +','+ sliderMerah.value +','+ sliderHijau.value +',1) 100%)';
+})
+
+sliderBiru.addEventListener('input', function(){
+  document.body.style.background = 'radial-gradient(circle, rgba('+ sliderMerah.value +','+ sliderHijau.value +','+ sliderBiru.value +',1) 0%, rgba('+ sliderBiru.value +','+ sliderMerah.value +','+ sliderHijau.value +',1) 100%)';
+})
